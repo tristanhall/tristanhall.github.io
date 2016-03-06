0e5479922291eb1e392808b86b3d5e6d7d1eb048
Tristan Hall <tristanhall100@gmail.com>
2016-03-05 23:16:11 -0500

    Add new HTML website.

---
+++ build-201603/config.rb
@@ -0,0 +1,9 @@
+
+css_dir = "css" # by Compass.app 
+sass_dir = "scss" # by Compass.app 
+images_dir = "images" # by Compass.app 
+output_style = :compressed # by Compass.app 
+relative_assets = true # by Compass.app 
+line_comments = false # by Compass.app 
+sass_options = {:debug_info=>false} # by Compass.app 
+sourcemap = true # by Compass.app 
